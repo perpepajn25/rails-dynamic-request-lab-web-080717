ae910308e1d08655602208c0e4d1a623fa753217
Natalie Perpepaj <perpepajn25@gmail.com>
2017-09-05 06:20:17 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,12 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+    'index'
   end
+
+  def show
+    @student = Student.find(params[:id])
+    'view'
+  end
+
 end
